dcc7c73faa14f0b2131b7fddaebf576dfde4434f
Lionel Miranda <bibliothecalionel@gmail.com>
2023-11-06 11:54:02 -0400

    feat: file_metadata function for file_process module

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -29,4 +29,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        queue = instance.search(position)
+        print(queue, file=sys.stdout)
+    except IndexError:
+        print('Posição inválida', file=sys.stderr)
